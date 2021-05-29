5f861a6d9465a0a7b848c445cd8df52a134bc14a
Epidra <epidra@web.de>
2021-05-29 14:32:59 +0200

    Release v01

--- src/main/java/mod/lucky77/Lucky77.java
+++ src/main/java/mod/lucky77/Lucky77.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+// The value here should match an entry in the META-INF/mods.toml
 @Mod("lucky77")
 public class Lucky77 {
 
